7e7cee5826ae34cb81e4c7c1f7523a29c90dc97c
ubu <dijouxd1993@gmail.com>
2020-04-22 14:44:47 +0200

    début

---
+++ mob/Recrutement/Creneau.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Recrutement
+{
+    public class Creneau
+    {
+        private readonly DateTime debut {get; private set;}
+        private readonly DateTime fin {get; private set;}
+        
+        
+    }
+}
---
+++ mob/Recrutement/Entretien.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Recrutement
+{
+    public class Entretien
+    {
+
+    }
+}
